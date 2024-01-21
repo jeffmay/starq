c3d01a9f8d35e88bd8b022c121554aa8b75ce195
Jeff May <jeff.may@calm.com>
2024-01-21 09:08:40 -0800

    ci(golangci-lint): 👷 Add `golangci-lint` to the build
    - 🔨 Add `lint.sh` to install `golangci-lint` with the installed go version
    - 👷 Add `.github/workflows/build.yaml` that calls `lint.sh`
    - 🔧 Add `.golangci.yaml` config for adding custom rules and documentation

---
+++ lint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Allow overriding the golangci-lint version or use 'latest'
+version="${GOLANGCI_VERSION:-latest}"
+
+# Set the bin path for the current Go environment
+gopath_bin="$(go env GOPATH)/bin"
+
+# Install golangci-lint if it's not already installed locally
+if [ -z "$(command -v $gopath_bin/golangci-lint)" ]; then
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@${version}
+fi
+
+# Run golangci-lint with github-actions format
+"$gopath_bin/golangci-lint" run -v --config .golangci.yaml --out-format github-actions ./...
